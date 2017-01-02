751cc3b5aa1ff99193b7dfebc0682e864322d243
AlexPurhalo <alexpurhalo@gmail.com>
2017-01-02 23:18:06 +0200

    my-solution

--- smallest-common-multiple.js
+++ smallest-common-multiple.js
@@ -30,10 +30,10 @@ function smallestCommons(arr) {
 
 	function findSameValue(biggestVal, lessVal) {
 		var initValBiggest = biggestVal, initValLess = lessVal;
-		var multVal = 1, biggestVals = [], keyVal = 0;
+		var multVal = 1, biggestVals = [], keyVal = false;
 
 
-		while(keyVal === 0) {
+		while(keyVal === false) {
 			biggestVal = biggestVal * multVal;
 			lessVal = lessVal * multVal;
 
@@ -59,7 +59,7 @@ function smallestCommons(arr) {
 		return keyVal;
 	}
 
-	console.log(result)
+	console.log(result);
 	return result
 }
 
@@ -67,10 +67,10 @@ function smallestCommons(arr) {
 // for 3 => 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60
 // for 4 => 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60
 // for 5 => 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60
-// smallestCommons([1, 5]);
+smallestCommons([1, 5]);
 //
 // smallestCommons([1, 5]); // a valber.
 // smallestCommons([1, 5]); // 60.
 // smallestCommons([5, 1]); // 60.
 // smallestCommons([1, 13]); // 360360.
-smallestCommons([23, 18]); // 6056820.
+// smallestCommons([23, 18]); // 6056820.
