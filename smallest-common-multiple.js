a18b6f7eca51756bb0014f660447f9f72a94ba35
AlexPurhalo <alexpurhalo@gmail.com>
2017-01-02 23:37:48 +0200

    smallest-common-multiple

--- smallest-common-multiple.js
+++ smallest-common-multiple.js
@@ -1,8 +1,7 @@
-// Smallest Common Multiple
-
 function smallestCommons(arr) {
-	var minVal = Math.min(arr[0], arr[1]), maxVal = Math.max(arr[0], arr[1]), result;
+	var minVal = Math.min(arr[0], arr[1]), maxVal = Math.max(arr[0], arr[1]), values;
 
+	// gives the values between arrays numbers (sorted from big to small) func(1, 3) => [3, 2, 1]
 	function getValues(minVal, maxVal) {
 		var val, vals = [];
 
@@ -13,64 +12,23 @@ function smallestCommons(arr) {
 		return vals
 	}
 
-	var values = getValues(minVal, maxVal), afterNextVal, sumTwoVals;
+	values = getValues(minVal, maxVal);
 
-	sumTwoVals = findSameValue(values[0], values[1]);
+	var resultVal = 0, multVal = 1, i = 0;
 
-	for (var i = 0; i <= values.length -1; i ++) {
-		afterNextVal = values[i+2];
+	while (i !== values.length) {
+		resultVal = values[0] * multVal * values[1];
 
-
-		if (afterNextVal !== undefined) {
-			if (sumTwoVals % afterNextVal !== 0) { sumTwoVals = findSameValue(sumTwoVals, afterNextVal) }
-			// console.log(sumTwoVals);
-			result = sumTwoVals
+		for (i = 2; i < values.length; i++) {
+			if (resultVal % values[i] !== 0) { break }
 		}
+
+		multVal++;
 	}
 
-	function findSameValue(biggestVal, lessVal) {
-		var initValBiggest = biggestVal, initValLess = lessVal;
-		var multVal = 1, biggestVals = [], keyVal = false;
 
-
-		while(keyVal === false) {
-			biggestVal = biggestVal * multVal;
-			lessVal = lessVal * multVal;
-
-			biggestVals.push(biggestVal);
-
-			if (searchVal(biggestVals, lessVal) === true) {
-				keyVal = lessVal;
-			}
-
-			biggestVal = initValBiggest;
-			lessVal = initValLess;
-
-			multVal += 1;
-		}
-
-		function searchVal(arr, val) {
-			for (var i = 0; i < arr.length; i++) {
-				if (val === arr[i]) { return true }
-			}
-			return false
-		}
-
-		return keyVal;
-	}
-
-	console.log(result);
-	return result
+	console.log(resultVal);
+	return resultVal;
 }
 
-// for 2 => 2, 4, 8, 10 ... 60
-// for 3 => 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60
-// for 4 => 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60
-// for 5 => 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60
-smallestCommons([1, 5]);
-//
-// smallestCommons([1, 5]); // a valber.
-// smallestCommons([1, 5]); // 60.
-// smallestCommons([5, 1]); // 60.
-// smallestCommons([1, 13]); // 360360.
-// smallestCommons([23, 18]); // 6056820.
+smallestCommons([1,5]);
